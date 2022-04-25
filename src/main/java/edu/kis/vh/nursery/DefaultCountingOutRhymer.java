80b0e711216e727da9eeb9eae6edd698b79cae89
236004 <236004@CARBON.CTI.P.LODZ.PL>
2022-04-25 12:56:16 +0200

    5.Dodanie stalych zamiast liczb w klasach

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private final int[] numbers = new int[12];
+    public static final int mnumber = 12;
+    public static final int mone = -1;
+    public static final int elev = 11;
+    private final int[] numbers = new int[mnumber];
 
-    public int total = -1;
+    public int total = mone;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +15,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == mone;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == elev;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return mone;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return mone;
         return numbers[total--];
     }
 
